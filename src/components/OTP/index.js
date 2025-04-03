a5b4b628f7cf820f21cb4dfc3ec9c9fad64f8f8e
Sandeep Kumar Shah <82407422+Sandeep96Shah@users.noreply.github.com>
2025-04-04 00:37:35 +0600

    Implemented otp input feature (#1)

---
+++ src/components/OTP/index.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useRef, useState } from "react";
+
+const OTP = () => {
+  const otpCount = 6;
+  const [inputList, setInputList] = useState(new Array(otpCount).fill(""));
+  const inputListRef = useRef([]);
+  const handleOnChange = (value, index) => {
+    if (isNaN(value)) return;
+    const trimmedValue = value?.trim();
+    const updatedInputList = [...inputList];
+    updatedInputList[index] = trimmedValue?.slice(-1);
+    setInputList(updatedInputList);
+    trimmedValue && inputListRef.current[index + 1]?.focus();
+  };
+
+  const handleOnBack = (e, index) => {
+    if (!e.target.value && e?.key === "Backspace")
+      inputListRef.current[index - 1]?.focus();
+  };
+
+  useEffect(() => {
+    inputListRef.current[0]?.focus();
+  }, []);
+
+  return (
+    <div>
+      <h1>OTP</h1>
+      {inputList?.map((input, index) => (
+        <input
+          key={index}
+          type="text"
+          value={inputList[index]}
+          className="otpInput"
+          onChange={(e) => handleOnChange(e?.target?.value, index)}
+          ref={(input) => (inputListRef.current[index] = input)}
+          onKeyDown={(e) => handleOnBack(e, index)}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default OTP;
