2cda1c6d2eea906dc42f76add6473166e5255f12
Sandeep Shah <sandeep2016shah@gmail.com>
2025-04-05 21:52:09 +0600

    implemented auto completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import TabForm from './components/TabForm';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import TablePagination from './components/TablePagination';
+import AutoComplete from './components/AutoComplete';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='/otp-input' element={<OTP />} />
         <Route path='tab-form' element={<TabForm />} />
         <Route path='table-pagination' element={<TablePagination />} />
+        <Route path='auto-complete' element={<AutoComplete />} />
       </Routes>
     </div>
   );
