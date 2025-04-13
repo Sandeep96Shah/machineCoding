a55b8833d4b7d661343c5e64a86fc684edcc794f
Sandeep Shah <sandeep2016shah@gmail.com>
2025-04-13 22:09:39 +0600

    updated logic to checked all the children if parent is checked

--- src/components/NestedCheckbox/index.jsx
+++ src/components/NestedCheckbox/index.jsx
@@ -1,78 +1,87 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { data } from "./data";
 import "./nestedCheckbox.css";
 
-const Checkbox = ({ data, checkboxResult, setCheckboxResult, }) => {
-    console.log({sandeep999: checkboxResult});
+const Checkbox = ({ data, checkboxResult, setCheckboxResult }) => {
+  console.log({ sandeep999: checkboxResult });
   return (
     <>
       {data.map((node) => {
-
-        const updateAllChildrenNode = ({data, isChecked}) => {
-            data?.forEach((node) => {
-                console.log({sandeepChildren: [node?.name]});
-                setCheckboxResult((prevChecked) => ({
-                ...prevChecked,
-                [node?.id]: isChecked
-                }))
-                if(node?.children){
-                    updateAllChildrenNode({data: node?.children, isChecked})
-                }
-            })
-        }
-        const handleOnChange = (e) => {
-            const isChecked = e?.target?.checked;
-          setCheckboxResult((prevResult) => ({
-            ...prevResult,
-            [node?.id]: isChecked,
-          }));
-          updateAllChildrenNode({data: node?.children, isChecked })
-          
+        const updateAllChildrenNode = ({
+          data,
+          isChecked,
+          updatedCheckedResult,
+        }) => {
+          data?.forEach((node) => {
+            console.log({ sandeepChildren: [node?.name] });
+            // setCheckboxResult((prevChecked) => ({
+            //   ...prevChecked,
+            //   [node?.id]: isChecked,
+            // }));
+            updatedCheckedResult[node?.id] = isChecked;
+            if (node?.children) {
+              updatedCheckedResult = updateAllChildrenNode({
+                data: node?.children,
+                isChecked,
+                updatedCheckedResult,
+              });
+            }
+            
+          });
+          return updatedCheckedResult;
         };
 
-        const getValue = ({data}) => {
-            let isChecked = false;
-            for(let i = 0; i < data?.length; i++){
-                console.log({sandeep1: "Inside GetValue", [data?.[i]?.name]: data?.[i]?.name});
-                if(!checkboxResult?.[data?.[i]?.id]){
-                   return false; 
-                }
-                isChecked = data?.[i]?.children ? getValue({data: data?.[i]?.children}) : true;
-                if(!isChecked) return false;
+        const getValue = ({ data }) => {
+          let isChecked = false;
+          for (let i = 0; i < data?.length; i++) {
+            console.log({
+              sandeep1: "Inside GetValue",
+              [data?.[i]?.name]: data?.[i]?.name,
+            });
+            if (!checkboxResult?.[data?.[i]?.id]) {
+              return false;
             }
-            // data?.forEach((node) => {
-            //     console.log({sandeep1: "Inside GetValue", [node?.name]: node?.name});
-            //     if(!checkboxResult?.[node?.id]){
-            //        return false; 
-            //     }
-            //     isChecked = node?.children ? getValue({data: node?.children}) : true;
-            //     if(!isChecked) return false;
-            // })
-            return isChecked;
-        }
+            isChecked = data?.[i]?.children
+              ? getValue({ data: data?.[i]?.children })
+              : true;
+            if (!isChecked) return false;
+          }
+          return isChecked;
+        };
+
+        const handleOnChange = (e) => {
+          const isChecked = e?.target?.checked;
+          //   setCheckboxResult((prevResult) => ({
+          //     ...prevResult,
+          //     [node?.id]: isChecked,
+          //   }));
+          //   updateAllChildrenNode({ data: node?.children, isChecked });
+          setCheckboxResult((prevResult) => {
+            let updatedCheckedResult = {
+              ...prevResult,
+              [node?.id]: isChecked,
+            };
+            updatedCheckedResult = updateAllChildrenNode({
+              data: node?.children,
+              isChecked,
+              updatedCheckedResult,
+            });
+            return updatedCheckedResult;
+          });
+        };
 
         const getDefaultValue = () => {
-            if(!node?.children){
-                return checkboxResult[node?.id]
-            }
-            return checkboxResult[node?.id]
-            console.log({[node?.name]: node?.children && getValue({data: node?.children})});
-            // if( node?.children && getValue({data: node?.children})) {
-            //     setCheckboxResult((prevResult) => ({
-            //         ...prevResult,
-            //         [node?.id]: true
-            //     }))
-            //     return true;
-            // }
-            return false;
-        }
+          if (!node?.children) {
+            return checkboxResult[node?.id];
+          }
+          return checkboxResult?.[node?.id];
+        };
         return (
           <div className="checkbox-container" key={node?.id}>
             <input
               type={"checkbox"}
               onChange={handleOnChange}
               checked={getDefaultValue()}
-              
             />
             <span>{node?.name}</span>
             {node?.children && (
