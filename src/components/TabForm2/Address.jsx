7e21ec491b8f0f580b379a077312f1aef1b4acae
Sandeep Shah <sandeep2016shah@gmail.com>
2025-05-22 14:43:49 +0600

    Optimise code

--- src/components/TabForm2/Address.jsx
+++ src/components/TabForm2/Address.jsx
@@ -1,46 +1,27 @@
 import React from "react";
 
 const Address = ({ defaultValues = {} }) => {
-  const { street = "", city = "", pincode = "", state = "" } = defaultValues;
+  const fields = [
+    { name: "street", label: "Street" },
+    { name: "city", label: "City" },
+    { name: "pincode", label: "Pincode" },
+    { name: "state", label: "State" },
+  ];
 
   return (
     <div>
-      <div>
-        <label htmlFor="street">Street</label>
-        <input
-          type="text"
-          defaultValue={street}
-          name="street"
-          id="street"
-          required
-        />
-      </div>
-
-      <div>
-        <label htmlFor="city">City</label>
-        <input type="text" defaultValue={city} name="city" id="city" required />
-      </div>
-
-      <div>
-        <label htmlFor="pincode">Pincode</label>
-        <input
-          type="text"
-          defaultValue={pincode}
-          name="pincode"
-          id="pincode"
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="state">state</label>
-        <input
-          type="text"
-          defaultValue={state}
-          name="state"
-          id="state"
-          required
-        />
-      </div>
+      {fields.map(({ name, label }) => (
+        <div key={name}>
+          <label htmlFor={name}>{label}</label>
+          <input
+            type="text"
+            defaultValue={defaultValues[name]}
+            name={name}
+            id={name}
+            required
+          />
+        </div>
+      ))}
     </div>
   );
 };
